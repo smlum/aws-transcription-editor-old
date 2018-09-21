01c27206c93f890c7523e45dce4c2d4b4d47d2c4
sam <samfredlumley@gmail.com>
2018-09-21 19:54:11 -0400

    Delete save.js

--- save.js
+++
@@ -1,9 +0,0 @@
-/*
-
-document.getElementById('heading').innerHTML = localStorage['title'] || 'Just Wrdcite'; // default text
-document.getElementById('content').innerHTML = localStorage['text'] || 'This text is automatically saved every second :) '; // default text
-
-setInterval(function() { // fuction that is saving the innerHTML of the div
-localStorage['title'] = document.getElementById('heading').innerHTML; // heading div
-localStorage['text'] = document.getElementById('content').innerHTML; // content div
-}, 1000);
